102d5f2326543005aa1051309d5aa6ad9fe176ce
Harshit <workwithvrz@gmail.com>
2026-03-02 19:18:16 +0530

    feat(config): add environment variable configuration and .env.example

---
+++ core/config.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    R2_ACCOUNT_ID: str
+    R2_ACCESS_KEY_ID: str
+    R2_SECRET_ACCESS_KEY: str
+    BUCKET_NAME: str 
+    SUPABASE_KEY: str
+    SUPABASE_URL: str
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
